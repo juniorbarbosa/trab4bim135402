4b87e20bae49e8e8e129cdcfd4534422b8210211
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-11-11 18:43:36 -0200

    Parte 07 - Criada uma nova tab para a importação de várias pessoas através de arquivo xml, alterando os arquivos CadastrarPessoaController.java para criar a função de upload do xml, e o arquivo cadastrarPessoa.xhtml para ter uma nova tab para essa importação, criado o arquivo ModeloXMLImportacaoPessoas.xml para utilização e exemplo de como o arquivo xml deve ser montado para ser possível a importação do xml.

--- Appjsf/src/main/java/br/com/juniorbarbosa/pessoa/controller/CadastrarPessoaController.java
+++ Appjsf/src/main/java/br/com/juniorbarbosa/pessoa/controller/CadastrarPessoaController.java
@@ -1,10 +1,21 @@
 package br.com.juniorbarbosa.pessoa.controller;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import br.com.juniorbarbosa.model.PessoaModel;
 import br.com.juniorbarbosa.repository.PessoaRepository;
@@ -31,6 +42,26 @@ public class CadastrarPessoaController implements Serializable {
 	@Inject
 	private PessoaRepository pessoaRepository;
 
+	private UploadedFile file;
+
+	/**
+	 * Obtém o arquivo xml
+	 * 
+	 * @return um <code>UploadedFile</code> especificando o arquivo xml
+	 */
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	/**
+	 * Indica um arquivo xml
+	 * 
+	 * @param file o arquivo xml
+	 */
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
 	/**
 	 * Obtém o a pessoa
 	 * 
@@ -50,7 +81,7 @@ public class CadastrarPessoaController implements Serializable {
 	}
 
 	/**
-	 * Método para salvar uma nova pessoa via input 
+	 * Método para salvar uma nova pessoa via input
 	 */
 	public void salvarNovaPessoa() {
 		pessoaModel.setUsuarioModel(this.usuarioController.getUsuarioSession());
@@ -58,6 +89,52 @@ public class CadastrarPessoaController implements Serializable {
 		pessoaRepository.salvarNovoRegistro(this.pessoaModel);
 		this.pessoaModel = null;
 		Uteis.mensagemInfo("Pessoa cadastrada com sucesso");
+	}
+
+	/**
+	 * Método que realiza o upload do arquivo xml e salva os registros no BD
+	 */
+	public void uploadRegistros() {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			if (this.file.getFileName().equals("")) {
+				Uteis.mensagemAtencao("Nenhum arquivo selecionado!");
+				return;
+			}
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(this.file.getInputstream());
+			Element element = doc.getDocumentElement();
+			NodeList nodes = element.getChildNodes();
+
+			for (int i = 0; i < nodes.getLength(); i++) {
+				Node node = nodes.item(i);
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element elementPessoa = (Element) node;
+					String nome = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0).getNodeValue();
+					String sexo = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0).getNodeValue();
+					String email = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue();
+					String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0).getNodeValue();
+
+					PessoaModel newPessoaModel = new PessoaModel();
+					newPessoaModel.setUsuarioModel(this.usuarioController.getUsuarioSession());
+					newPessoaModel.setEmail(email);
+					newPessoaModel.setEndereco(endereco);
+					newPessoaModel.setNome(nome);
+					newPessoaModel.setOrigemCadastro("X");
+					newPessoaModel.setSexo(sexo);
+
+					pessoaRepository.salvarNovoRegistro(newPessoaModel);
+				}
+			}
+			Uteis.mensagemInfo("Registros cadastrados com sucesso!");
+			
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
